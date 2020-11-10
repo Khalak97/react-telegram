29c4072c2017af3083ee6caa5dd8898638b4a332
Khalak97 <valodyakh@gmail.com>
2020-11-10 14:10:18 +0200

    add db

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
 import "./App.css";
 
+import Telegram from "./components/Telegram";
+import Login from "./components/Login";
+
+import { auth } from "./firebase";
+import { login, logout, selectUser } from "./features/userSlice";
+
 function App() {
-  return <div className="app">React app - telegram...</div>;
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        dispatch(
+          login({
+            uid: authUser.uid,
+            photo: authUser.photoURL,
+            email: authUser.email,
+            displayName: authUser.displayName,
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+  }, []);
+  return <div className="app">{user ? <Telegram /> : <Login />}</div>;
 }
 
 export default App;
